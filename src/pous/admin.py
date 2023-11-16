062f3ce0098731dab28be9d3cb6be23e16954767
Fred Thomas <fred.thomas@ouce.ox.ac.uk>
2023-11-16 15:43:58 +0000

    Correctly index state

--- src/pous/admin.py
+++ src/pous/admin.py
@@ -11,7 +11,7 @@ def us_county_name(county_code: str, county_boundaries: gpd.GeoDataFrame, states
         county_admin_data: pd.Series = \
             county_boundaries.sort_values("GEOID").set_index("GEOID").loc[county_code, :]
         state_code: str = county_admin_data.STATEFP
-        state_name: str = states.loc[int(state_code), "state_name"]
+        state_name: str = states.loc[states["state_fips_code"] == int(state_code), "state_name"]
         county_name: str = county_admin_data.NAME
     except Exception as e:
         state_name = "-"
